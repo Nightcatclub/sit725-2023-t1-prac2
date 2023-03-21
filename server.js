16092466cd8649070a4461f82c41fee35cb02ce3
ROY <101548778+Nightcatclub@users.noreply.github.com>
2023-03-21 15:24:18 +1100

    fix

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require("express");
 var app = express();
 var port = process.env.port || 3000;
 
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
+
 function complexMath(num1, num2) {
   return parseInt(num1) + parseInt(num2);
 }
@@ -10,7 +18,7 @@ app.get("/addTwoNumbers", (req, res) => {
   var number1 = req.query.number1;
   var number2 = req.query.number2;
   var result = complexMath(number1, number2);
-  res.json({ statusCode: 200, data: result, message: "Success plus" });
+  res.json({ statusCode: 200, data: result, message: "Success" });
 });
 
 app.listen(port, () => {
